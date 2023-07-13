0a1d5465cd7f64243e6f59e241e91cc838aaa836
Zakariae El Kouch <zelkouch@emeal.nttdata.com>
2023-07-13 15:28:46 +0100

    update test_api

--- app/tests/test_api.py
+++ app/tests/test_api.py
@@ -1,8 +1,7 @@
 import pytest
 import json
 from app import app
-from models.project_user import Project_user
-import static
+
 
 def test_index_route():
     response = app.test_client().get('/')
@@ -17,9 +16,3 @@ def test_get_name_route():
     assert response.status_code == 200
     assert name == 'SE4I project'
 
-def test_get_dec_route():
-    response = app.test_client().get('/get-dec')
-    data = json.loads(response.data)
-    dec = data[static.data_list]
-    assert response.status_code == 200
-    assert dec == 'original from Spain'
