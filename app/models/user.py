0ac16b21099d676772640143d2370b7e7066add6
RidaYasserYr <yasser.rida@emeal.nttdata.com>
2023-07-13 06:26:10 +0100

    update user Model

--- app/models/user.py
+++ app/models/user.py
@@ -4,41 +4,47 @@ import json
 
 class User:
 
-    def __init__(self) -> None:
-        self.users = []
-        data = json.load(open('app/static/users_list.json'))
-        if (len(data)):
-            self.users = data
+    def __init__(self, id, firstName, lastName, email, password, birthDate) -> None:
+        self.id = id
+        self.firstName = firstName
+        self.lastName = lastName
+        self.email = email
+        self.password = password
+        self.birthDate = birthDate
 
-    def get_full_name(self, user):
-        return "{firstName} {lastName}".format(firstName=user['firstName'], lastName=user['lastName'].upper())
+    def get_full_name(self):
+        return "{firstName} {lastName}".format(firstName=self.firstName, lastName=self.lastName.upper())
 
-    def get_age(self, user):
+    def get_age(self):
         today = date.today()
-        birthDate = datetime.strptime(user['birthDate'], '%d-%m-%Y')
+        birthDate = datetime.strptime(self.birthDate, '%d-%m-%Y')
         return today.year - birthDate.year - ((today.month, today.day) < (birthDate.month, birthDate.day))
 
-    def store(self, id, firstName, lastName, email, password, birthDate):
-        self.users.append({
-            "id": id,
-            "firstName": firstName,
-            "lastName": lastName,
-            "email": email,
-            "password": password,
-            "birthDate": birthDate
+    def store(self):
+        data = json.load(open('app/static/users_list.json'))
+        users_list = data if (len(data)) else []
+        users_list.append({
+            "id": self.id,
+            "firstName": self.firstName,
+            "lastName": self.lastName,
+            "email": self.email,
+            "password": self.password,
+            "birthDate": self.birthDate
         })
         with open('app/static/users_list.json', 'w') as outfile:
-            json.dump(self.users, outfile)
+            json.dump(users_list, outfile)
 
-    def update(self, id, firstName, lastName, email, password, birthDate):
-        for user in self.users:
-            if user['id'] == id:
-                user['firstName'] = firstName
-                user['lastName'] = lastName
-                user['email'] = email
-                user['password'] = password
-                user['birthDate'] = birthDate
+    def update(self):
+        data = json.load(open('app/static/users_list.json'))
+        users_list = data if (len(data)) else []
+        for user in users_list:
+            if user['id'] == self.id:
+                user['firstName'] = self.firstName
+                user['lastName'] = self.lastName
+                user['email'] = self.email
+                user['password'] = self.password
+                user['birthDate'] = self.birthDate
                 with open('app/static/users_list.json', 'w') as outfile:
-                    json.dump(self.users, outfile)
+                    json.dump(users_list, outfile)
                 return
         raise Exception("Not existe")
