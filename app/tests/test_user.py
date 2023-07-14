996b60aacce4fd781876abbc8b46ce7f8b70543d
RidaYasserYr <yasser.rida@emeal.nttdata.com>
2023-07-14 11:17:23 +0100

    update user tests

--- app/tests/test_user.py
+++ app/tests/test_user.py
@@ -3,23 +3,44 @@ import json
 
 
 def test_index_route():
-    response = app.test_client().get('/users')
+    response = app.test_client().get("/users")
 
     assert response.status_code == 200
-    assert len(json.loads(response.data.decode('utf-8')))
+    assert len(json.loads(response.data.decode("utf-8")))
 
 
 def test_show_route():
-    response = app.test_client().get('/users/1')
+    response = app.test_client().get("/users/1")
 
     assert response.status_code == 200
 
 
 def test_update_route():
-    response = app.test_client().put('/users/1', data={"firstName": "test1",
-                                                  "lastName": "test1",
-                                                  "email": "test1@nttdata.com",
-                                                  "password": "654321",
-                                                  "birthDate": "01/01/1999"})
+    response = app.test_client().put(
+        "/users/2",
+        json={
+            "firstName": "test1",
+            "lastName": "test1",
+            "email": "test1@nttdata.com",
+            "password": "654321",
+            "birthDate": "01/01/1999",
+        },
+    )
+
+    assert response.status_code == 200
+
+
+def test_store_route():
+    response = app.test_client().post(
+        "/users",
+        json={
+            "id": 4,
+            "firstName": "test14",
+            "lastName": "test4",
+            "email": "test4@nttdata.com",
+            "password": "654321",
+            "birthDate": "01/01/1998",
+        },
+    )
 
     assert response.status_code == 200
