7fb1655959c0638a7644ce7afb17326de5ebd267
Mohamed Ali El Hassouni <melhasso@emeal.nttdata.com>
2023-07-14 12:47:03 +0100

    Adding film_controller.py, film.py, films_list.json

---
+++ app/models/film.py
@@ -0,0 +1,11 @@
+from app import app
+import json
+
+class Film:
+    def __init__(self):
+        self.films_list = []
+        parsed_films = json.load(open('app/static/users_list.json'))
+        if len(parsed_films):
+            self.films_list = parsed_films
+
+
