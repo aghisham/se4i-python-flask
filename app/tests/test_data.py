f0cb8174b9b74dc61ce1cab53335d90e424780f5
Zakariae El Kouch <zelkouch@emeal.nttdata.com>
2023-07-25 16:24:30 +0100

    update test_data

---
+++ app/tests/test_data.py
@@ -0,0 +1,53 @@
+import pytest
+import json
+from app import app
+import static
+ 
+
+def test_get_dec_route():
+    response = app.test_client().get('/get-dec')
+    data = json.loads(response.data)
+    for x in static.data_list:
+     dec = data["desc"]
+     assert response.status_code == 200
+     assert dec == 'original from Spain'
+
+def test_get_R():
+    response = app.test_client().get("/data/1")
+    assert response.status_code == 200
+
+
+def test_post_R():
+    
+     response = app.test_client().post(
+        "/data",
+        json= {
+              "id":1,
+              "brand": "Ford",
+              "model": "Mustang",
+              "year": 1964,
+              "dec":"original from America"
+       },
+      )
+
+     assert response.status_code == 200
+    
+
+
+def test_put_R():
+     response = app.test_client().post(
+           "/data/1",
+            json= {
+              "id":1,
+              "brand": "Ford",
+              "model": "Mustang",
+              "year": 1964,
+              "dec":"original from America"
+            },
+          )
+     assert response.status_code == 200
+
+
+def test_delete_R():
+    response = app.test_client().delete("/data/1")
+    assert response.status_code == 200
