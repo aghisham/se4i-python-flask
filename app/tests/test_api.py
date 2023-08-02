d7f4f92d9a33a9aec31ebd4a2d562b4f6e66ee24
Hicham Agourame <hagouram@emeal.nttdata.com>
2023-08-02 20:47:17 +0100

    update: tests for posts_mongo_controller

--- app/tests/test_api.py
+++ app/tests/test_api.py
@@ -18,24 +18,24 @@ def test_get_name_route():
 
 
 def test_get():
-    response = app.test_client().get("/items/1")
+    response = app.test_client().get("/api/items/1")
     assert response.status_code == 200
 
 
 def test_post():
     response = app.test_client().post(
-        "/items", json={"title": "Test Title", "body": "Test Body", "userId": 1}
+        "/api/items", json={"title": "Test Title", "body": "Test Body", "userId": 1}
     )
     assert response.status_code == 201
 
 
 def test_put():
     response = app.test_client().put(
-        "/items/1", json={"title": "Updated Title", "body": "Updated Body", "userId": 1}
+        "/api/items/1", json={"title": "Updated Title", "body": "Updated Body", "userId": 1}
     )
     assert response.status_code == 200
 
 
 def test_delete():
-    response = app.test_client().delete("/items/1")
+    response = app.test_client().delete("/api/items/1")
     assert response.status_code == 200
